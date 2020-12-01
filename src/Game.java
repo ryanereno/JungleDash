8af2dd63cc67ae9b30a7c252d5f3d77a738e2751
Ryan Ereno <ryanaereno@gmail.com>
2020-11-30 16:43:28 -0800

    Created the screen in the game class

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,25 @@
+import javax.swing.JFrame;
+import java.awt.*;
+
+
 public class Game {
 
-    public static void main(String[] args) {
+    static GraphicsConfiguration gc;
 
+    public static void main(String[] args) {
+       drawWindow();
+
+    }
+
+    // We would paste the objects into this method to draw them?
+    // so eventually this method will take in the objects of our games
+    // as parameters so we could draw them
+    public static void drawWindow(){
+        JFrame frame = new JFrame(gc);
+        frame.setTitle("Jungle Dash!");
+        frame.setSize(2000,2000);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
     }
 }
